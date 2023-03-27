e0552381578ff5f1c83c1112044597398bbc933f
Almuhannad Ghaleb <henno.noman@gmail.com>
2023-03-27 22:22:16 +0300

    refactor: extract theme

--- lib/src/widgets/credit_card.dart
+++ lib/src/widgets/credit_card.dart
@@ -4,13 +4,13 @@ import 'package:flutter/services.dart';
 import 'package:moyasar/moyasar.dart';
 import 'package:moyasar/src/moyasar.dart';
 
-import 'package:moyasar/src/models/card_form_model.dart';
 import 'package:moyasar/src/models/payment_request.dart';
 import 'package:moyasar/src/models/sources/card/card_request_source.dart';
 
 import 'package:moyasar/src/utils/card_utils.dart';
 import 'package:moyasar/src/utils/input_formatters.dart';
-import 'package:moyasar/src/widgets/network_icons.dart';
+import 'package:moyasar/src/utils/themes/credit_form_theme.dart';
+import 'package:moyasar/src/widgets/card_form_field.dart';
 import 'package:moyasar/src/widgets/three_d_s_webview.dart';
 
 /// The widget that shows the Credit Card form and manages the 3DS step.
@@ -39,8 +39,7 @@ class _CreditCardState extends State<CreditCard> {
   bool isSubmitting = false;
 
   void _saveForm() async {
-    closeKeyboard();
-
+    FocusManager.instance.primaryFocus?.unfocus();
     bool isValidForm =
         _formKey.currentState != null && _formKey.currentState!.validate();
 
@@ -104,7 +103,7 @@ class _CreditCardState extends State<CreditCard> {
       child: Column(
         children: [
           CardFormField(
-              inputDecoration: buildInputDecoration(
+              inputDecoration: CreditFormTheme.buildInputDecoration(
                 hintText: widget.locale.nameOnCard,
               ),
               keyboardType: TextInputType.text,
@@ -115,7 +114,7 @@ class _CreditCardState extends State<CreditCard> {
                 FilteringTextInputFormatter.allow(RegExp('[a-zA-Z. ]')),
               ]),
           CardFormField(
-            inputDecoration: buildInputDecoration(
+            inputDecoration: CreditFormTheme.buildInputDecoration(
                 hintText: widget.locale.cardNumber, addNetworkIcons: true),
             validator: (String? input) =>
                 CardUtils.validateCardNum(input, widget.locale),
@@ -128,7 +127,7 @@ class _CreditCardState extends State<CreditCard> {
                 _cardData.number = CardUtils.getCleanedNumber(value!),
           ),
           CardFormField(
-            inputDecoration: buildInputDecoration(
+            inputDecoration: CreditFormTheme.buildInputDecoration(
               hintText: '${widget.locale.expiry} (MM / YY)',
             ),
             inputFormatters: [
@@ -145,7 +144,7 @@ class _CreditCardState extends State<CreditCard> {
             },
           ),
           CardFormField(
-            inputDecoration: buildInputDecoration(
+            inputDecoration: CreditFormTheme.buildInputDecoration(
               hintText: widget.locale.cvc,
             ),
             inputFormatters: [
@@ -172,7 +171,7 @@ class _CreditCardState extends State<CreditCard> {
                         color: Colors.white,
                         strokeWidth: 2,
                       )
-                    : Text(showAmount(widget.config.amount, widget.locale)),
+                    : Text(_showAmount(widget.config.amount, widget.locale)),
               ),
             ),
           ),
@@ -180,74 +179,14 @@ class _CreditCardState extends State<CreditCard> {
       ),
     );
   }
-}
 
-class CardFormField extends StatelessWidget {
-  final void Function(String?)? onSaved;
-  final String? Function(String?)? validator;
-  final TextInputType keyboardType;
-  final TextInputAction textInputAction;
-  final List<TextInputFormatter>? inputFormatters;
-  final InputDecoration? inputDecoration;
+  String _showAmount(int amount, Localization locale) {
+    final formattedAmount = (amount / 100).toStringAsFixed(2);
 
-  const CardFormField({
-    Key? key,
-    required this.onSaved,
-    this.validator,
-    this.inputDecoration,
-    this.keyboardType = TextInputType.number,
-    this.textInputAction = TextInputAction.next,
-    this.inputFormatters,
-  }) : super(key: key);
+    if (locale.languageCode == 'en') {
+      return '${locale.pay} SAR $formattedAmount';
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: TextFormField(
-          keyboardType: keyboardType,
-          textInputAction: textInputAction,
-          decoration: inputDecoration,
-          validator: validator,
-          onSaved: onSaved,
-          inputFormatters: inputFormatters),
-    );
+    return '${locale.pay} $formattedAmount ر.س';
   }
 }
-
-String showAmount(int amount, Localization locale) {
-  final formattedAmount = (amount / 100).toStringAsFixed(2);
-
-  if (locale.languageCode == 'en') {
-    return '${locale.pay} SAR $formattedAmount';
-  }
-
-  return '${locale.pay} $formattedAmount ر.س';
-}
-
-InputDecoration buildInputDecoration(
-    {required String hintText, bool addNetworkIcons = false}) {
-  return InputDecoration(
-      suffixIcon: addNetworkIcons ? const NetworkIcons() : null,
-      hintText: hintText,
-      focusedErrorBorder: defaultErrorBorder,
-      enabledBorder: defaultEnabledBorder,
-      focusedBorder: defaultFocusedBorder,
-      errorBorder: defaultErrorBorder);
-}
-
-void closeKeyboard() => FocusManager.instance.primaryFocus?.unfocus();
-
-BorderRadius defaultBorderRadius = const BorderRadius.all(Radius.circular(8));
-
-OutlineInputBorder defaultEnabledBorder = OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.grey[400]!),
-    borderRadius: defaultBorderRadius);
-
-OutlineInputBorder defaultFocusedBorder = OutlineInputBorder(
-    borderSide: BorderSide(color: Colors.grey[600]!),
-    borderRadius: defaultBorderRadius);
-
-OutlineInputBorder defaultErrorBorder = OutlineInputBorder(
-    borderSide: const BorderSide(color: Colors.red),
-    borderRadius: defaultBorderRadius);

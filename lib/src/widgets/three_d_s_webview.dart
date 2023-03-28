2b27246d774aff4412e06b44e92c8d54e77cf999
Almuhannad Ghaleb <henno.noman@gmail.com>
2023-03-28 22:36:16 +0300

    refactor: move actions to external functions

--- lib/src/widgets/three_d_s_webview.dart
+++ lib/src/widgets/three_d_s_webview.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 class ThreeDSWebView extends StatefulWidget {
   final String transactionUrl;
@@ -9,9 +8,9 @@ class ThreeDSWebView extends StatefulWidget {
 
   const ThreeDSWebView(
       {super.key,
-        required this.transactionUrl,
-        required this.callbackUrl,
-        required this.on3dsDone});
+      required this.transactionUrl,
+      required this.callbackUrl,
+      required this.on3dsDone});
 
   @override
   State<ThreeDSWebView> createState() => _ThreeDSWebViewState();
@@ -23,66 +22,24 @@ class _ThreeDSWebViewState extends State<ThreeDSWebView> {
   void initState() {
     super.initState();
 
-    // #docregion platform_features
-    late final PlatformWebViewControllerCreationParams params;
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
+    _controller = WebViewController.fromPlatformCreationParams(
+        const PlatformWebViewControllerCreationParams());
 
-    final WebViewController controller =
-    WebViewController.fromPlatformCreationParams(params);
-    // #enddocregion platform_features
-
-    controller
+    _controller
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(Colors.white)
       ..setNavigationDelegate(
         NavigationDelegate(
-          onProgress: (int progress) {
-            debugPrint('WebView is loading (progress : $progress%)');
-          },
-          onPageStarted: (String url) {
-            debugPrint('Page started loading: $url');
-          },
-          onPageFinished: (String url) {
-            debugPrint('Page finished loading: $url');
-            final redirectedTo = Uri.parse(url);
-            final callbackUri = Uri.parse(widget.callbackUrl);
-
-            final bool hasReachedFinalRedirection =
-                redirectedTo.host == callbackUri.host;
-
-            if (hasReachedFinalRedirection) {
-              final queryParams = redirectedTo.queryParameters;
-
-              String? status = queryParams['status'];
-              String? message = queryParams['message'];
-
-              widget.on3dsDone(status, message);
-            }
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('''
-                Page resource error:
-                  code: ${error.errorCode}
-                  description: ${error.description}
-                  errorType: ${error.errorType}
-                  isForMainFrame: ${error.isForMainFrame}
-                  ''');
-          },
+          onProgress: _onProgress,
+          onPageStarted: _onPageStarted,
+          onPageFinished: _onPageFinished,
+          onWebResourceError: _onWebResourceError,
           onNavigationRequest: (NavigationRequest request) {
             return NavigationDecision.navigate;
           },
         ),
       )
       ..loadRequest(Uri.parse(widget.transactionUrl));
-
-    _controller = controller;
   }
 
   @override
@@ -100,5 +57,38 @@ class _ThreeDSWebViewState extends State<ThreeDSWebView> {
         ),
       ),
     );
+  }
+
+  void _onProgress(int progress) {
+    debugPrint('WebView is loading (progress : $progress%)');
+  }
+
+  void _onPageStarted(String url) {
+    debugPrint('Page started loading: $url');
+  }
+
+  void _onPageFinished(String url) {
+    debugPrint('Page finished loading: $url');
+    final redirectedTo = Uri.parse(url);
+    final callbackUri = Uri.parse(widget.callbackUrl);
+    final bool hasReachedFinalRedirection =
+        redirectedTo.host == callbackUri.host;
+
+    if (hasReachedFinalRedirection) {
+      final queryParams = redirectedTo.queryParameters;
+      String? status = queryParams['status'];
+      String? message = queryParams['message'];
+      widget.on3dsDone(status, message);
+    }
+  }
+
+  void _onWebResourceError(WebResourceError error) {
+    debugPrint('''
+                Page resource error:
+                  code: ${error.errorCode}
+                  description: ${error.description}
+                  errorType: ${error.errorType}
+                  isForMainFrame: ${error.isForMainFrame}
+                  ''');
   }
 }

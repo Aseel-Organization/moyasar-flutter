2ca87770241a72b3574138768e119fe50d0fe6d1
Almuhannad Ghaleb <henno.noman@gmail.com>
2023-04-06 12:52:40 +0300

    refactor: remove unused models

--- lib/moyasar.dart
+++ lib/moyasar.dart
@@ -7,6 +7,7 @@ export 'src/widgets/apple_pay.dart' show ApplePay;
 export 'src/widgets/credit_card_button.dart' show CreditCardButton;
 
 export 'src/models/payment_config.dart' show PaymentConfig;
+export 'package:pay/pay.dart' show PaymentConfiguration;
 export 'src/models/payment_response.dart' show PaymentResponse, PaymentStatus;
 export 'src/models/card_form_model.dart' show CardFormModel;
 export 'src/services/moyasar_service.dart' show MoyasarService;
@@ -16,8 +17,6 @@ export 'src/models/sources/card/card_response_source.dart'
     show CardPaymentResponseSource;
 export 'src/models/sources/apple_pay/apple_pay_response_source.dart'
     show ApplePayPaymentResponseSource;
-export 'src/models/apple_pay_configuration.dart'
-    show ApplePayConfiguration, SupportedNetworks, MerchantCapabilities;
 
 export 'src/locales/localizaton.dart' show Localization;
 
